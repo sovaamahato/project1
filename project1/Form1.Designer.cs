f550c2e75babbb99670561efde06d862043099da
sovaamahato <mahatoshobha143@gmail.com>
2022-11-22 09:40:33 +0545

    first project

--- project1/Form1.Designer.cs
+++ project1/Form1.Designer.cs
@@ -28,12 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Username = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Userage = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(12, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // Username
+            // 
+            this.Username.Location = new System.Drawing.Point(59, 47);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(194, 23);
+            this.Username.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(81, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 51);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter your details here.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Age";
+            // 
+            // Userage
+            // 
+            this.Userage.Location = new System.Drawing.Point(59, 83);
+            this.Userage.Name = "Userage";
+            this.Userage.Size = new System.Drawing.Size(194, 23);
+            this.Userage.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(399, 66);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(241, 132);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Userage);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "form";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox Username;
+        private Button button1;
+        private Label label2;
+        private Label label3;
+        private TextBox Userage;
+        private PictureBox pictureBox1;
     }
 }
--- project1/Form1.cs
+++ project1/Form1.cs
@@ -6,5 +6,27 @@ namespace project1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string userName = Username.Text;
+            int i = 0;
+            int UserAge = Convert.ToInt32(Userage.Text);
+            if (UserAge>= 18)
+            {
+                MessageBox.Show("You are elligible");
+
+            }
+            else
+            {
+                MessageBox.Show("You are not elligible");
+
+            }
+            for (i = 0; i < 3; i++)
+            {
+                MessageBox.Show("Your name is " + userName);
+            }
+           
+        }
     }
 }
